8e97d95ade1e4535c126ed00a36a36e754bf6e6c
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-26 18:30:38 +0500

    added homework_14

---
+++ homework_14.js
@@ -0,0 +1,15 @@
+function myFilter(array, callback) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
+const numbers = [1, 2, 3, 4, 5, 45, 100, 0, -100, -30, 13];
+
+const callback = (item) => item <= 3;
+console.log(myFilter(numbers, callback));
+console.log(numbers);
